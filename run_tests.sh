02b86511a08963633a211fa344abbeed0cc9dd77
Thiago Giachetto de Araujo <thiago.giachetto@gmail.com>
2024-06-18 13:30:30 +0200

    update run_tests and create a new script to aggregate all experiments on a folder

--- run_tests.sh
+++ run_tests.sh
@@ -2,11 +2,11 @@
 
 MODELS="FlowModel3-base FlowModel3-valid-ineq MTZ-sbpo FlowModel2-1-base FlowModel2-1 MCFModel MCFModel-base REPR-mtz-base REPR-mtz REPR-flow-base REPR-flow MAR-mtz-base MAR-mtz MAR-flow-base MAR-flow"
 
-for model in $MODELS_TEST; do
+for model in $MODELS; do
     for instance in $(ls instances); do
         for i in {1..8}; do
             echo $instance - $model - $i
-            ./build/all_models -i instances/$instance -m $model -K $i
+            ./build/all_models -i instances/$instance -m $model -K $i -r
             # ./build/all_models -i instances/den20mtsNodes_20.txt -m $model -K $i
         done
     done
