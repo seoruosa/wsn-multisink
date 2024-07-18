d5adc749a15e0c75554be2efadfc6d06adda18e8
Thiago Giachetto de Araujo <thiago.giachetto@gmail.com>
2024-07-18 18:54:11 +0200

    removing commented code

--- include/models/WSN_mcf_weight_arc.h
+++ include/models/WSN_mcf_weight_arc.h
@@ -190,9 +190,6 @@ inline void WSN_mcf_weight_arc_model::add_ahani2019_mcf_constraints()
         clear_expr();
     }
 
-    // constraints.add(expr == instance.number_trees);
-    // clear_expr();
-
     // constraints 6 and 7
     for (int i = 0; i < instance.n; i++)
     {
@@ -389,35 +386,6 @@ inline void WSN_mcf_weight_arc_model::add_flow_conservation_constraints()
 {
     IloExpr expr(env);
 
-    // for (int s = 0; s < instance.number_trees; s++)
-    // {
-    //     for (int i = 0; i < instance.n; i++)
-    //     {
-
-    //         expr += f_sink[s][instance.n][i];
-
-    //         for (auto &from : instance.adj_list_to_v[i])
-    //         {
-    //             expr += f_sink[s][from][i];
-    //         }
-
-    //         for (auto &to : instance.adj_list_from_v[i])
-    //         {
-    //             expr -= f_sink[s][i][to];
-    //         }
-
-    //         for (auto &from : instance.adj_list_to_v[i])
-    //         {
-    //             expr -= instance.weight[from][i] * x_sink[s][from][i];
-    //         }
-
-    //         constraints.add(expr == 0);
-
-    //         expr.end();
-    //         expr = IloExpr(env);
-    //     }
-    // }
-
     for (int i = 0; i < instance.n; i++)
     {
         for (int s = 0; s < instance.number_trees; s++)
@@ -463,12 +431,6 @@ inline void WSN_mcf_weight_arc_model::add_CastroAndrade2023_valid_inequalities()
             // constraint 16 and 17
             constraints.add(x[i][j] <= z[i] + z[j]);
             constraints.add(x[i][j] <= y[i] + y[j]);
-
-            // for (int k = 0; k < instance.number_trees; k++)
-            // {
-            //     constraints.add(x_sink[k][i][j] <= z_sink[k][i] + z_sink[k][j]);
-            //     constraints.add(x_sink[k][i][j] <= y_sink[k][i] + y_sink[k][j]);
-            // }
         }
     }
 }
@@ -487,10 +449,6 @@ inline void WSN_mcf_weight_arc_model::add_adasme2023_valid_inequalities()
             {
                 // constraint 20
                 constraints.add(y[from] + y[i] == 1);
-
-                // constraint 21
-                // constraints.add(2 * z[from] <= y[i] + x[from][i]);
-                // constraints.add(2 * (x[from][i] + x[i][from]) <= y[i] + z[from]);
             }
         }
     }
@@ -525,76 +483,6 @@ inline void WSN_mcf_weight_arc_model::add_adasme2023_valid_inequalities()
     }
 
     exp_ad_28.end();
-
-    // IloExpr exp_ad_47(env);
-    // for (int i = 0; i < instance.n; i++)
-    // {
-    //     for (auto &from : instance.adj_list_to_v[i])
-    //     {
-    //         for (int h = 0; h < instance.n; h++)
-    //         {
-    //             exp_ad_47 += f_sink[h][from][i];
-    //         }
-    //     }
-
-    //     // Constraints 47
-    //     constraints.add(2 * z[i] <= exp_ad_47);
-
-    //     exp_ad_47.end();
-    //     exp_ad_47 = IloExpr(env);
-    // }
-    // exp_ad_47.end();
-
-    // IloExpr exp_ad_48(env);
-    // for (int i = 0; i < instance.n; i++)
-    // {
-    //     for (auto &j : instance.adj_list_from_v[i])
-    //     {
-    //         for (int h = 0; h < instance.n; h++)
-    //         {
-    //             exp_ad_48 += f_sink[h][i][j];
-    //         }
-
-    //         // Constraints 48
-    //         constraints.add(2 * x[i][j] - z[i] <= exp_ad_48);
-
-    //         exp_ad_48.end();
-    //         exp_ad_48 = IloExpr(env);
-    //     }
-    // }
-    // exp_ad_48.end();
-
-    // IloExpr exp_ad_49(env);
-    // IloExpr exp_ad_49_1(env);
-    // for (int i = 0; i < instance.n; i++)
-    // {
-    //     for (int h = 0; h < instance.n; h++)
-    //     {
-    //         for (auto &from : instance.adj_list_to_v[i])
-    //         {
-    //             exp_ad_49 += f_sink[h][from][i];
-    //         }
-    //         exp_ad_49 += f_sink[h][instance.n][i];
-    //     }
-
-    //     for (auto &j : instance.adj_list_from_v[i])
-    //     {
-    //         for (int h = 0; h < instance.n; h++)
-    //         {
-    //             exp_ad_49_1 += f_sink[h][i][j];
-    //         }
-    //         // Constraints 49
-    //         constraints.add(exp_ad_49_1 + x[i][j] * (instance.n - 1) - exp_ad_49 <= instance.n - 2);
-
-    //         exp_ad_49_1.end();
-    //         exp_ad_49_1 = IloExpr(env);
-    //     }
-
-    //     exp_ad_49.end();
-    //     exp_ad_49 = IloExpr(env);
-    // }
-    // exp_ad_49.end();
-    // exp_ad_49_1.end();
 }
 
 void WSN_mcf_weight_arc_model::add_mcf_valid_inequalities()
